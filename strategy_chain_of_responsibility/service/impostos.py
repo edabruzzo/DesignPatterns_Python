3fd2da851ca9ea0a78b12649b40573310c70acf0
Emmanuel Abruzzo <emmanuel.oliveira3@gmail.com>
2021-11-29 11:40:29 -0300

    Inclui novos impostos para aplicar Template Method

--- strategy_chain_of_responsibility/service/impostos.py
+++ strategy_chain_of_responsibility/service/impostos.py
@@ -8,7 +8,7 @@ class Imposto(object):
     def calcular(self, orcamento, aliquota):
         imposto = 0
         try:
-            imposto = orcamento.valor_total * 0.06
+            imposto = orcamento.valor_total * aliquota
         except Exception as erro:
             print(erro)
             return 0
@@ -16,10 +16,37 @@ class Imposto(object):
 
 
 class ISS(Imposto):
+
     def   calcula(self, orcamento):
         return Imposto.calcular(self,  orcamento=orcamento, aliquota=0.1)
 
 
 class ICMS(Imposto):
+
     def calcula(self, orcamento):
         return Imposto.calcular(self, orcamento=orcamento, aliquota=0.06)
+
+
+class ICPP(Imposto):
+
+    def calcula(self, orcamento):
+        if orcamento.valor_total > 500:
+            return Imposto.calcular(self, orcamento=orcamento, aliquota=0.07)
+        else:
+            return Imposto.calcular(self, orcamento=orcamento, aliquota=0.05)
+
+
+class ICPP(Imposto):
+
+    def calcula(self, orcamento):
+        if orcamento.valor_total > 500 and self.__orcamento_possui_mais_de_um_item(orcamento):
+            return Imposto.calcular(self, orcamento=orcamento, aliquota=0.1)
+        else:
+            return Imposto.calcular(self, orcamento=orcamento, aliquota=0.08)
+
+
+    def __orcamento_possui_item_maior_cem_reais(self, orcamento):
+        for item in orcamento.itens:
+            if item.valor > 100:
+                return True
+        return False
