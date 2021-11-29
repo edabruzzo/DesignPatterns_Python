a8dd1a76e951abe4b48f3e0652d41a40269611dd
Emmanuel Abruzzo <emmanuel.oliveira3@gmail.com>
2021-11-29 08:58:11 -0300

    Define o modelo de orcamento

--- strategy_chain_of_responsibility/model/orcamento.py
+++ strategy_chain_of_responsibility/model/orcamento.py
@@ -5,9 +5,34 @@ class Orcamento(object):
     def __init__(self):
         self.__itens = []
 
+
+    @property
+    def itens(self):
+        return self.__itens
+
+    @itens.setter
+    @property
+    def itens(self, itens):
+        self.__itens = itens
+
+    @property
+    def valor(self):
+        total = 0.0
+        for item in self.__itens:
+            total += item.valor()
+        return total
+
+
     def obter_itens(self):
         return tuple(self.__)
 
+    @property
+    def total_itens(self):
+        return len(self.__itens)
+
+
+    def add_item(self, item):
+        self.__itens.append(item)
 
 
 class Item(object):
